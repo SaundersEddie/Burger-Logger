5deaf366c9fadb35459ad729fee309882990e8c0
Eddie Saunders <saunders.eddie@outlook.com>
2020-05-18 17:35:06 -0400

    fixing dumb arse VS Code auto complete bollocks

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-if (process.nextTick.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
